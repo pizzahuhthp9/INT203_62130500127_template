074b1d86bd36e4f86b7f2d74d6e89fc05cc37a36
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-02-10 12:44:19 +0700

    init

--- my-template/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/css/tailwind.css'
-import router from './router/index.js'
-
-createApp(App).use(router)
-.mount('#app')
